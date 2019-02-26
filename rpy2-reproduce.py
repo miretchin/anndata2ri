c10b3435ff5b5726b9c48a1eda87b313fcdcab8f
Philipp A <flying-sheep@web.de>
2019-02-26 13:03:18 +0100

    smaller

--- rpy2-reproduce.py
+++ rpy2-reproduce.py
@@ -13,17 +13,11 @@ def gc():
 
 def reproduced():
 	from rpy2.robjects import numpy2ri, r
-	from rpy2.robjects.packages import importr
 
-	se = importr("SummarizedExperiment")
-
-	sce_allen = r('''
-	suppressPackageStartupMessages(library(SingleCellExperiment))
+	exprs = numpy2ri.rpy2py(r('''
 	data(allen, package="scRNAseq")
-	as(allen, 'SingleCellExperiment')
-	''')
-
-	exprs = numpy2ri.rpy2py(se.assay(sce_allen, 1)).T
+	SummarizedExperiment::assay(allen, 1L)
+	''')).T
 	gc()
 	print(exprs)
 
