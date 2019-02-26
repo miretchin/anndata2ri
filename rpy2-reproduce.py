b0a39cde88be40b2e52493f43df707314b7585d1
Philipp A <flying-sheep@web.de>
2019-02-26 13:12:07 +0100

    comments

--- rpy2-reproduce.py
+++ rpy2-reproduce.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
+import gc
 import faulthandler
-from gc import collect as py_gc
 
 from rpy2.robjects import numpy2ri, baseenv, r
 
+
+# SEGFAULT tracebacks are a godsend.
 faulthandler.enable()
-r_gc = baseenv['gc']
 
-
-def gc():
-	py_gc()
-	r_gc()
-
-
+# Convert an R matrix to a numpy array
+# without keeping a reference on the R side.
+# It needs to be either big enough or data-backed(?)
 exprs = numpy2ri.rpy2py(r('''
 data(allen, package="scRNAseq")
 SummarizedExperiment::assay(allen, 1L)
-''')).T
-gc()
-print(exprs)
+'''))
 
+# Garbage collect in Python and R.
+gc.collect()
+baseenv['gc']()
+
+# SEGFAULT when interacting with the numpy array.
+print(exprs)
