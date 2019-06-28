ca65c178115ebe3be793b5f43936bb31af571290
miretchin <miretchin@users.noreply.github.com>
2019-06-27 21:22:44 -0400

    Update __init__.py

--- anndata2ri/__init__.py
+++ anndata2ri/__init__.py
@@ -16,29 +16,22 @@ __version__ = get_version(__file__)
 del get_version
 
 
-try:  # This is so that flit can import this. There must be a better way.
-    from .conv import converter, activate, deactivate
-    from . import py2r, r2py
+from .conv import converter, activate, deactivate
+from . import py2r, r2py
 
-    def py2rpy(obj: Any) -> Sexp:
-        """
-        Convert Python objects to R interface objects. Supports:
+def py2rpy(obj: Any) -> Sexp:
+    """
+    Convert Python objects to R interface objects. Supports:
 
-        - :class:`~anndata.AnnData` → :rcls:`SingleCellExperiment::SingleCellExperiment`
-        """
-        return converter.py2rpy(obj)
+    - :class:`~anndata.AnnData` → :rcls:`SingleCellExperiment::SingleCellExperiment`
+    """
+    return converter.py2rpy(obj)
 
-    def rpy2py(obj: Any) -> Sexp:
-        """
-        Convert R interface objects to Python objects. Supports:
+def rpy2py(obj: Any) -> Sexp:
+    """
+    Convert R interface objects to Python objects. Supports:
 
-        - :rcls:`SingleCellExperiment::SingleCellExperiment` → :class:`~anndata.AnnData`
-        - :rcls:`S4Vectors::DataFrame` → :class:`~pandas.DataFrame`
-        """
-        return converter.rpy2py(obj)
-
-
-except ImportError as e:
-    import warnings
-
-    warnings.warn(str(e), ImportWarning)
+    - :rcls:`SingleCellExperiment::SingleCellExperiment` → :class:`~anndata.AnnData`
+    - :rcls:`S4Vectors::DataFrame` → :class:`~pandas.DataFrame`
+    """
+    return converter.rpy2py(obj)
